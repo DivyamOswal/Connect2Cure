b339a586470520591768833d459bd77b3543a133
Divyam Mukesh Oswal <divyam9850400550@gmail.com>
2026-02-11 16:09:28 +0530

    language_code_added to features section

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -2,13 +2,20 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+// Navbar translations
 import enNavbar from "./locales/en/navbar.json";
 import hiNavbar from "./locales/hi/navbar.json";
 import mrNavbar from "./locales/mr/navbar.json";
+
+// Hero translations
 import enHero from "./locales/en/hero.json";
 import hiHero from "./locales/hi/hero.json";
 import mrHero from "./locales/mr/hero.json";
 
+// Home Features translations
+import enFeatures from "./locales/en/homefeatures.json";
+import hiFeatures from "./locales/hi/homefeatures.json";
+import mrFeatures from "./locales/mr/homefeatures.json";
 
 i18n
   .use(LanguageDetector)
@@ -17,22 +24,32 @@ i18n
     resources: {
       en: {
         navbar: enNavbar,
-        hero: enHero
+        hero: enHero,
+        features: enFeatures
       },
       hi: {
         navbar: hiNavbar,
-        hero: hiHero
+        hero: hiHero,
+        features: hiFeatures
       },
       mr: {
         navbar: mrNavbar,
-        hero: mrHero
+        hero: mrHero,
+        features: mrFeatures
       }
     },
-    fallbackLng: "en",
-    ns: ["navbar", "hero"],
-    defaultNS: "navbar",
-    interpolation: { escapeValue: false }
-  });
 
+    fallbackLng: "en",
+
+    // ALL namespaces used in the app
+    ns: ["navbar", "hero", "features"],
+
+    // Default namespace (Navbar uses this)
+    defaultNS: "navbar",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
 export default i18n;
